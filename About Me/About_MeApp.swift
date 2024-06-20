98d613f208f9409073bb66d25dfc5022b2f3faba
vivianwang <vvccwang@gmail.com>
2024-06-20 14:42:59 -0400

    Initial Commit

---
+++ About Me/About_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_MeApp.swift
+//  About Me
+//
+//  Created by Vivian Wang on 2024-06-20.
+//
+
+import SwiftUI
+
+@main
+struct About_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About Me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  About Me
+//
+//  Created by Vivian Wang on 2024-06-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
